5ff2dbcc53925d84008c51c9c41a8b2050910efe
EdgarMarJara <79977475+EdgarMarJara@users.noreply.github.com>
2021-04-06 22:20:26 -0400

    Tarea de push/pop

---
+++ [COLAS] Push y pop/[COLAS] Push y pop/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace _COLAS__Push_y_pop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.t4 = new System.Windows.Forms.TextBox();
+            this.t3 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.t5 = new System.Windows.Forms.TextBox();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.btnquitar = new System.Windows.Forms.Button();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.t6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // t4
+            // 
+            this.t4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.t4.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t4.Location = new System.Drawing.Point(100, 200);
+            this.t4.Multiline = true;
+            this.t4.Name = "t4";
+            this.t4.Size = new System.Drawing.Size(68, 48);
+            this.t4.TabIndex = 0;
+            this.t4.Text = "99";
+            this.t4.Visible = false;
+            // 
+            // t3
+            // 
+            this.t3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.t3.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t3.Location = new System.Drawing.Point(190, 200);
+            this.t3.Multiline = true;
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(68, 48);
+            this.t3.TabIndex = 1;
+            this.t3.Text = "99";
+            this.t3.Visible = false;
+            // 
+            // t2
+            // 
+            this.t2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.t2.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t2.Location = new System.Drawing.Point(280, 200);
+            this.t2.Multiline = true;
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(68, 48);
+            this.t2.TabIndex = 2;
+            this.t2.Text = "99";
+            this.t2.Visible = false;
+            // 
+            // t5
+            // 
+            this.t5.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.t5.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t5.Location = new System.Drawing.Point(10, 200);
+            this.t5.Multiline = true;
+            this.t5.Name = "t5";
+            this.t5.Size = new System.Drawing.Size(68, 48);
+            this.t5.TabIndex = 3;
+            this.t5.Text = "99";
+            this.t5.Visible = false;
+            // 
+            // t1
+            // 
+            this.t1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.t1.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t1.Location = new System.Drawing.Point(370, 200);
+            this.t1.Multiline = true;
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(68, 48);
+            this.t1.TabIndex = 4;
+            this.t1.Text = "99";
+            this.t1.Visible = false;
+            // 
+            // btnquitar
+            // 
+            this.btnquitar.Location = new System.Drawing.Point(363, 96);
+            this.btnquitar.Name = "btnquitar";
+            this.btnquitar.Size = new System.Drawing.Size(75, 23);
+            this.btnquitar.TabIndex = 5;
+            this.btnquitar.Text = "Eliminar";
+            this.btnquitar.UseVisualStyleBackColor = true;
+            this.btnquitar.Click += new System.EventHandler(this.btnquitar_Click);
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(273, 96);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(75, 23);
+            this.btnagregar.TabIndex = 6;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // t6
+            // 
+            this.t6.BackColor = System.Drawing.SystemColors.Info;
+            this.t6.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t6.Location = new System.Drawing.Point(190, 79);
+            this.t6.Multiline = true;
+            this.t6.Name = "t6";
+            this.t6.Size = new System.Drawing.Size(68, 48);
+            this.t6.TabIndex = 7;
+            this.t6.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 19);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Total de elementos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.t6);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.btnquitar);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.t5);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox t4;
+        private System.Windows.Forms.TextBox t3;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.TextBox t5;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.Button btnquitar;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.TextBox t6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ [COLAS] Push y pop/[COLAS] Push y pop/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _COLAS__Push_y_pop
+{
+    public partial class Form1 : Form
+    {
+        int td = 0;
+        Random objeto = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            if (td == 5)
+            {
+                MessageBox.Show ("La Cola esta llena");
+            }
+            else
+            {
+                td++;
+                t6.Text = Convert.ToString(td);
+                if(td == 1)
+                {
+                    t1.Text = Convert.ToString(objeto.Next(1, 99));
+                    t1.Visible = true;
+                }
+                if (td == 2)
+                {
+                    t2.Text = Convert.ToString(objeto.Next(1, 99));
+                    t2.Visible = true;
+                }
+                if (td == 3)
+                {
+                    t3.Text = Convert.ToString(objeto.Next(1, 99));
+                    t3.Visible = true;
+                }
+                if (td == 4)
+                {
+                    t4.Text = Convert.ToString(objeto.Next(1, 99));
+                    t4.Visible = true;
+                }
+                if (td == 5)
+                {
+                    t5.Text = Convert.ToString(objeto.Next(1, 99));
+                    t5.Visible = true;
+                }
+            }
+        }
+
+        private void btnquitar_Click(object sender, EventArgs e)
+        {
+            if (td == 0) MessageBox.Show("La cola esta vacia weon");
+
+            else 
+            {
+                t1.Text = t2.Text;
+                t2.Text = t3.Text;
+                t3.Text = t4.Text;
+                t4.Text = t5.Text;
+                if (td == 1) t1.Visible = false;
+                if (td == 2) t2.Visible = false;
+                if (td == 3) t3.Visible = false;
+                if (td == 4) t4.Visible = false;
+                if (td == 5) t5.Visible = false;
+                td--;
+                t6.Text = Convert.ToString(td);
+            }
+        }
+    }
+}
