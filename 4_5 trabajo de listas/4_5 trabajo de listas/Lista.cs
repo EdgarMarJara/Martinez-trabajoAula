58564db1104b5793ad2968a0ff65fc208337ef25
EdgarMarJara <79977475+EdgarMarJara@users.noreply.github.com>
2021-05-05 21:12:22 -0400

    Trabajo de listas

---
+++ 4_5 trabajo de listas/4_5 trabajo de listas/Lista.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_5_trabajo_de_listas
+{
+    class Lista 
+    {
+        public int Insertar()
+        {
+            var seed = Environment.TickCount;
+            var random = new Random(seed);
+            var value = random.Next(0, 25);
+            return value;
+        }
+    }
+}
---
+++ 4_5 trabajo de listas/4_5 trabajo de listas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _4_5_trabajo_de_listas
+{
+    class Program
+    {
+        static void Main()
+        {
+            int valor = 0;
+            int suma = 0;
+            float pmd = 0;
+            Console.WriteLine("buenasss!!!");
+            Lista listalol = new Lista();
+            for (int i = 0; i <=25; i++)
+            {
+                listalol.Insertar();
+                valor = listalol.Insertar();
+                suma = suma + valor;
+                Console.WriteLine($"El numero que entra es: {valor}");
+            }
+            pmd = suma / 100;
+            Console.WriteLine($"La suma es: {suma}");
+            Console.WriteLine($"El promedio es: {pmd}");
+            Console.WriteLine("Press any key para salir");
+            Console.ReadKey();
+        }
+    }
+}
